5cf5057f978562e9859afdbab813278d2f6bd21d
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-22 17:12:04 -0700

    initial commits

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 /*
-Package **dast8x8** provides a set of **8×8 sprites** for the character names **DAST**,
-that seamlessly works with Go's built-in `"image"`, `"image/color"`, and `"image/draw"` packages.
+Package dast8x8 provides a set of 8×8 sprites for the character names DAST,
+that seamlessly works with Go's built-in "image", "image/color", and "image/draw" packages.
 */
 package dast8x8
